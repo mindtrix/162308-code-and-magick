30350a8cc624eac5400c0b9da1ff211ef3c58b8b
mindtrix <mindtrix@ya.ru>
2017-04-05 02:19:16 +0300

    'add_function_cloud,function_textInCloud,fix_histogramDinHeight_problem_whith_text_values_and_colors,exsept_alfa_chanel_'

--- js/stats.js
+++ js/stats.js
@@ -1,44 +1,53 @@
 'use strickt'
 
 window.renderStatistics = function(ctx, names, times) {
-	
-	ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-	ctx.fillRect(110,20,420,270);//Сделать функцию с пармаетрами цвета и корордлинаты
-	ctx.fillStyle = 'white';
-	ctx.fillRect(100,10,420,270);
+	function cloud(color, indent) { 
+	ctx.fillStyle = color;
+	ctx.fillRect(100 + indent, 10 + indent, 420, 270);
+	};
+	cloud('rgba(0, 0, 0, 0.7)', 10);
+	cloud('white', 0);
+
 	ctx.fillStyle = '#000';
 	ctx.font='16px PT Mono';
-	ctx.fillText('Ура, вы победили!', 140, 40);
-	ctx.fillText('Список результатов: ', 140, 58);
-	
+
+	function textInCloud(text, indentX, indentY) {
+	ctx.fillText(text, indentX, 40 + indentY);
+	ctx.fillText(text, indentX, 40 + indentY);
+	};
+	textInCloud('Ура, вы победили!', 140, 0);
+	textInCloud('Список результатов: ', 140, 20);
 	
 	var max = 0;
-	var indexMax = 0;
-	for (var i = 0; i <= times.length; ++i) { //Находим все эл-ты массива.
-		var time = times[i];//
-		if(times[i] > max) { //Ищем наибольшее значение в times[]; //Почему не работает if(times[i] > max){}?
-			max = times[i]; //Передаем значение в var max;
-		}
+	for (var i = 0; i <= times.length; ++i) { 
+		var time = times[i];
+		if(times[i] > max) { 
+			max = times[i]; 
 		};
-
+	};
 
 	var histagramHeight = 120;
 	var histogramDinHeight = histagramHeight / max; 
 	var histagramWidth = 40;
 	var histagramIndent = 90;
-	var randomNumber = Math.random();
-	var myColor = 'rgba(255, 0, 0, 1)';
-	var playersColor = 'rgba(0, 0, 255, randomNumber)';
+	var myColor = 'rgba(255, 0, 0, 1)'; 
+	var playersColor = 'rgba(0, 0, 255, Math.random())';
 
 	for (var i = 0; i < times.length; i++) {
-	
-		ctx.fillRect(150 + histagramIndent * i, 240, histagramWidth, histogramDinHeight * -times[i]);
+		function dinColor() {
+			if(names[i]!='Вы')  {
+			ctx.fillStyle = playersColor
+		}
+		else {
+			ctx.fillStyle = myColor;
+		};
+	};
+		ctx.fillStyle = "#000";
 		ctx.fillText(names[i], 150 + histagramIndent * i, 260);
-		ctx.fillStyle = 'blue';
-		ctx.fillText(times[i].toFixed(0), 150 + histagramIndent * i, histogramDinHeight * -times[i]); 
-
-	}
-		 
+		ctx.fillText(times[i].toFixed(0), 150 + histagramIndent * i,240 - histogramDinHeight * times[i] - 20); 
+		ctx.fillStyle = dinColor();
+		ctx.fillRect(150 + histagramIndent * i, 240, histagramWidth, histogramDinHeight * -times[i]);
+	};
 };
 
 
