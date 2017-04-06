1adfe3441876a13934dc5f03e8132d09646661ea
mindtrix <mindtrix@ya.ru>
2017-04-06 12:07:56 +0300

    Edit_codestyle

--- js/stats.js
+++ js/stats.js
@@ -1,10 +1,10 @@
-'use strickt'
+'use strickt';
 
 window.renderStatistics = function(ctx, names, times) {
 	function cloud(color, indent) { 
-	ctx.fillStyle = color;
-	ctx.fillRect(100 + indent, 10 + indent, 420, 270);
-	};
+		ctx.fillStyle = color;
+		ctx.fillRect(100 + indent, 10 + indent, 420, 270);
+	}
 	cloud('rgba(0, 0, 0, 0.7)', 10);
 	cloud('white', 0);
 
@@ -12,9 +12,9 @@ window.renderStatistics = function(ctx, names, times) {
 	ctx.font='16px PT Mono';
 
 	function textInCloud(text, indentX, indentY) {
-	ctx.fillText(text, indentX, 40 + indentY);
-	ctx.fillText(text, indentX, 40 + indentY);
-	};
+		ctx.fillText(text, indentX, 40 + indentY);
+		ctx.fillText(text, indentX, 40 + indentY);
+	}
 	textInCloud('Ура, вы победили!', 140, 0);
 	textInCloud('Список результатов: ', 140, 20);
 	
@@ -23,8 +23,8 @@ window.renderStatistics = function(ctx, names, times) {
 		var time = times[i];
 		if(times[i] > max) { 
 			max = times[i]; 
-		};
-	};
+		}
+	}
 
 	var histagramHeight = 120;
 	var histogramDinHeight = histagramHeight / max; 
@@ -34,20 +34,20 @@ window.renderStatistics = function(ctx, names, times) {
 	var playersColor = 'rgba(0, 0, 255, Math.random())';
 
 	for (var i = 0; i < times.length; i++) {
-		function dinColor() {
+		var dinColor = function () {
 			if(names[i]!='Вы')  {
-			ctx.fillStyle = playersColor
-		}
-		else {
-			ctx.fillStyle = myColor;
+				ctx.fillStyle = playersColor;
+			}
+			else {
+				ctx.fillStyle = myColor;
+			}
 		};
-	};
-		ctx.fillStyle = "#000";
+		ctx.fillStyle = '#000';
 		ctx.fillText(names[i], 150 + histagramIndent * i, 260);
 		ctx.fillText(times[i].toFixed(0), 150 + histagramIndent * i,240 - histogramDinHeight * times[i] - 20); 
 		ctx.fillStyle = dinColor();
 		ctx.fillRect(150 + histagramIndent * i, 240, histagramWidth, histogramDinHeight * -times[i]);
-	};
+	}
 };
 
 
