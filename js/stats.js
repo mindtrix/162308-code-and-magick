555107c73162dacc3ad929e59c38b8fad7103605
mindtrix <mindtrix@ya.ru>
2017-04-07 22:32:15 +0300

    restuckt

--- js/stats.js
+++ js/stats.js
@@ -1,9 +1,9 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
-  function cloud(color, indent) {
+	function cloud(color, indent) {
 	ctx.fillStyle = color;
 	ctx.fillRect(100 + indent, 10 + indent, 420, 270);
-	}
+}
 	function textInCloud(text, indentX, indentY) {
 		ctx.fillStyle = '#000';
 		ctx.font = '16px PT Mono';
@@ -19,29 +19,29 @@ window.renderStatistics = function (ctx, names, times) {
 			}
 			return max;
 		}
-	var histagramHeight = 120;
-	var histogramDinHeight = histagramHeight / maxTime();
-	var histagramWidth = 40;
-	var histagramIndent = 90;
-	var myColor = 'rgba(255, 0, 0, 1)';
-	var playersColor = function () {
+		var histagramHeight = 120;
+		var histogramDinHeight = histagramHeight / maxTime();
+		var histagramWidth = 40;
+		var histagramIndent = 90;
+		var myColor = 'rgba(255, 0, 0, 1)';
+		var playersColor = function () {
 		return 'rgba(0, 0, 255,' + Math.random() + ')';
-		};
-			for (var i = 0; i < times.length; i++) {
+	};
+		for (var i = 0; i < times.length; i++) {
 				var dinColor = function () {
-				if (names[i] !== 'Вы') {
+					if (names[i] !== 'Вы') {
 					ctx.fillStyle = playersColor();
 				} else {
 					ctx.fillStyle = myColor;
 				}
-			};
-		ctx.fillStyle = '#000';
-		ctx.fillText(names[i], 150 + histagramIndent * i, 260);
-		ctx.fillText(times[i].toFixed(0), 150 + histagramIndent * i, 240 - histogramDinHeight * times[i] - 20);
-		ctx.fillStyle = dinColor();
-		ctx.fillRect(150 + histagramIndent * i, 240, histagramWidth, histogramDinHeight * -times[i]);
+				};
+			ctx.fillStyle = '#000';
+			ctx.fillText(names[i], 150 + histagramIndent * i, 260);
+			ctx.fillText(times[i].toFixed(0), 150 + histagramIndent * i, 240 - histogramDinHeight * times[i] - 20);
+			ctx.fillStyle = dinColor();
+			ctx.fillRect(150 + histagramIndent * i, 240, histagramWidth, histogramDinHeight * -times[i]);
 			}
-	};
+	}
 	cloud('rgba(0, 0, 0, 0.7)', 10);
 	cloud('white', 0);
 	textInCloud('Ура, вы победили!', 140, 0);
